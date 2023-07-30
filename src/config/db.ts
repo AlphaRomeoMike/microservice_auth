3662a5f281d6fa9051183905eb5586ae8f68a9be
AlphaRomeoMike <74718326+AlphaRomeoMike@users.noreply.github.com>
2023-07-30 19:51:08 +0500

    Configured DB middleware for handling soft deletes

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import config from "./index";
+import { set } from "lodash";
 
 console.log(`\u2713 Primsa DB URL is ${config.DATABASE_URL}`)
 const client = new PrismaClient({
@@ -8,6 +9,21 @@ const client = new PrismaClient({
       url: config.DATABASE_URL
     }
   }
+})
+
+client.$use(async (params, next) => {
+  if (params.model == 'User') {
+    if (params.action == 'delete') {
+      set(params, 'action', 'update')
+      params.args['data'] = { deletedAt: new Date() }
+      params.args['data'] = { updatedAt: new Date() }
+    }
+    if (params.action == 'update') {
+      params.args['data'] = { updatedAt: new Date() }
+    }
+  }
+
+  return next(params);
 })
 
 export { client }
